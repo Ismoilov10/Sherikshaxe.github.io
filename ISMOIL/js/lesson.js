d6f9a606bb13e9e9647b71b83e43ec529a88ddaf
Sherikshaxe <34997904+Sherikshaxe@users.noreply.github.com>
2018-01-16 20:41:19 +0300

    Updated js/effects
    Added effects

--- ISMOIL/js/lesson.js
+++ ISMOIL/js/lesson.js
@@ -1,6 +1,6 @@
 $(function(){
   
-     $("body").on("keyup",".rakam", function(){
+     /*$("body").on("keyup",".rakam", function(){
        
        var text = $("#text").val();
 
@@ -41,19 +41,19 @@ $(function(){
      }); 
 
 
+*/
 
 
 
-
-    $("body").on("click","#btn",function(){
+    $("body").on("click",".btn",function(){
        
        var random = Math.random()*4;
        var random = Math.floor(random)
        $(".box").css("background-color","green")
        $("#box_"+random).css("background-color","black")
 });
-$("body").on("click","#btn",function(){
-    var words = ["Manchester City","Manchester United","Barcelona","Real Madrid da josh Rid","Arsenal","Chelsea","Bayern Munchёn","Juventus","PSG","Atletico Madrid","BVB	"]
+$("body").on("click",".btn",function(){
+    var words = ["Manchester City","Manchester United","Barcelona","Real Madrid","Arsenal","Chelsea","Bayern Munchёn","Juventus","PSG","Atletico Madrid","BVB	"]
        $("#word")
      var hisob = {
      	random:random
@@ -62,9 +62,53 @@ $("body").on("click","#btn",function(){
       function random(rakam){
       	rakam = Math.random()*12;
       	rakam = Math.floor(rakam);
-      	$("#word").html(words[rakam])
+      	$(".word").html("Да "+words[rakam])
       }
       hisob.random(4)
-      });    
-  });
+      });  
+
+
+                  var doIt = $(".btn").html()
+
+            points();
+            var timer = 0
+            function points(){
+
+            timer++
+
+            if(timer==1){
+              $(".btn").html($(".btn").html()+".")
+              }
+            if(timer==2){
+              $(".btn").html($(".btn").html()+".")
+              } 
+              if(timer==3){
+              $(".btn").html($(".btn").html()+".")
+              }
+              if(timer==4){
+              $(".btn").html(doIt)
+              timer=0
+              } 
+
+                setTimeout(points,650)
+                }
+
+
+                          //  LIGHTNING FUNCS //             
+            lights()
+            var lightstimer = 0
+            function lights(){
+
+            lightstimer++
+
+            if(lightstimer==99999){
+                lightstimer=0
+            } 
+            $(".lightbox").css("background-color","#"+lightstimer)
+            setTimeout(lights,1)
+            }
+
+
+
+});
 
